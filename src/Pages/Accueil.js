0dcbeaae003ae82a650392829b238de93f481966
Djibril <djibrilsamassa.pro@gmail.com>
2023-06-03 19:16:40 +0200

    loader

--- src/Pages/Accueil.js
+++ src/Pages/Accueil.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 import React from "react";
 import ProductCard from "../Composants/ProductCard";
 import { useState, useEffect } from "react";
@@ -9,8 +9,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Accueil() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [categoryDescription, setCategoryDescription] = useState(null);
   // Je recrée une liste filtrée pour ne pas interragir directement avec la liste de base et qu'on puisse y éventullement y revenir
   const [listToDisplay, setListToDisplay] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // recuperer les catégories et produits au chargement de la page
   useEffect(() => {
@@ -33,12 +35,23 @@ export default function Accueil() {
 
   // filtrer par catégorie
   const handleFilter = (catId) => {
+    setLoading(true);
     const filteredList = products.filter(
       (product) => product.category_id === catId
     );
 
     setListToDisplay(filteredList);
+
+    axios
+      .get(`https://test-feed.airweb.workers.dev/categories/${catId}`)
+      .then((res) => {
+        setCategoryDescription(res.data);
+      });
   };
+
+  useEffect(() => {
+    categoryDescription !== null && setLoading(false);
+  }, [categoryDescription]);
 
   return (
     <div className={Style.container}>
@@ -70,12 +83,18 @@ export default function Accueil() {
               variant="danger"
               onClick={() => {
                 setListToDisplay(products);
+                setCategoryDescription(null);
               }}
             >
               Enelever le filtre
             </Button>
           ) : null}
         </nav>
+        {loading ? (
+          <div className={Style.customLoader}></div>
+        ) : categoryDescription !== null ? (
+          <p>{categoryDescription.description}</p>
+        ) : null}
         {/* la liste des produits filtrée ou non */}
         {listToDisplay.map((produit) => {
           return <ProductCard key={produit.id} produit={produit} />;
